54a080438afa249206dd54460b8034761b2f1267
Prakash <prakash398prajapati@gmail.com>
2024-11-04 10:44:29 +0530

    all new git

---
+++ Scripts/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.textContent1').classList.add('visible');
+    }, 2000);
+    setTimeout(function() {
+        document.querySelector('.imageContent').classList.add('visible');
+    }, 4000);
+};
